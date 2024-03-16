b49b7b032d64435f7e19909bf585efd4d4128395
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-16 20:42:19 -0300

    funcao com parametros

---
+++ funcparametrizada.js
@@ -0,0 +1,27 @@
+const defautlValue = 0
+
+const sum  = (a = defautlValue, b = defautlValue) => { // pode asusmir valores padroes
+    a = Number(a)
+    b = Number(b)
+    return a + b
+}
+
+let valor = 0
+
+function add(v){
+    return valor + v
+}
+
+console.log(valor)
+valor = add(10)
+console.log(valor)
+valor = add(5)
+console.log(valor)
+
+
+// console.log(sum(2, 8))
+// console.log(sum('4', 4))
+// console.log(sum(2, 123))
+// console.log(sum(2))
+
+
