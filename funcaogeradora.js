2b9372dc82000e752db3889420d226c7a2d17923
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-16 22:02:04 -0300

    generator function

--- funcaogeradora.js
+++ funcaogeradora.js
@@ -1,26 +1,34 @@
-// function* perguntas(){
-//     const nome = yield 'Qual seu nome?'
-//     const esporte = yield 'Qual o seu esporte favorito?'
-//     return `Seu nome eh ${nome}, seu esporte favorito eh ${esporte}`
+function* perguntas(){
+    const nome = yield 'Qual seu nome?' //...
+
+    //a pergunta é feita e a função fica em espera ("await"), quando ela é chamada novamente o valor de entrada da chamada vai para a constante anterior.
+    
+    const esporte = yield 'Qual o seu esporte favorito?'
+    return `Seu nome eh ${nome}, seu esporte favorito eh ${esporte}`
+}
+
+const itp = perguntas()
+
+console.log(itp.next().value)
+console.log(itp.next('Bruno').value)
+console.log(itp.next('Basquete').value)
+
+
+
+
+
+
+// function* contador(){
+//     let i = 0
+//     while(true){
+//         yield i++
+//         if(i > 50){
+//             break
+//         }
+//     }
 // }
+// const itc = contador()
 
-// const itp = perguntas()
-
-// console.log(itp.next().value)
-// console.log(itp.next('Bruno').value)
-// console.log(itp.next('Basquete').value)
-
-function* contador(){
-    let i = 0
-    while(true){
-        yield i++
-        if(i > 50){
-            break
-        }
-    }
-}
-const itc = contador()
-
-for(c of itc){
-    console.log(c)
-}
+// for(c of itc){
+//     console.log(c)
+// }
