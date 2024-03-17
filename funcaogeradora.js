d811b2dcd43eaa4b362df1af359b8dda2136d12e
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-16 21:57:34 -0300

    generator function

--- funcaogeradora.js
+++ funcaogeradora.js
@@ -1,11 +1,26 @@
-function* cores(){
-    yield 'Vermelho'
-    yield 'Verde'
-    yield 'Azul'
+// function* perguntas(){
+//     const nome = yield 'Qual seu nome?'
+//     const esporte = yield 'Qual o seu esporte favorito?'
+//     return `Seu nome eh ${nome}, seu esporte favorito eh ${esporte}`
+// }
+
+// const itp = perguntas()
+
+// console.log(itp.next().value)
+// console.log(itp.next('Bruno').value)
+// console.log(itp.next('Basquete').value)
+
+function* contador(){
+    let i = 0
+    while(true){
+        yield i++
+        if(i > 50){
+            break
+        }
+    }
 }
+const itc = contador()
 
-const itc = cores()
-
-console.log(itc.next().value)
-console.log(itc.next().value)
-console.log(itc.next().value)
+for(c of itc){
+    console.log(c)
+}
