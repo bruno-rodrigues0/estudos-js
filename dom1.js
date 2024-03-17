fa4c00adbf3438f422c4c50825090d814de97a05
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-17 10:22:51 -0300

    getElementsByTagName e HTMLColections => Array

--- dom1.js
+++ dom1.js
@@ -1,3 +1,5 @@
+//getElementById
+
 const div1 = document.getElementById("c1")
 const div2 = document.getElementById("c2")
 const div3 = document.getElementById("c3")
@@ -6,8 +8,18 @@ const div5 = document.getElementById("c5")
 const div6 = document.getElementById("c6")
 
 
-const array = [...document.querySelectorAll('div')]
+const array = [div1, div2, div3, div4, div5, div6]
 
-array.map(item => {
-    item.innerHTML += " C eh loco tiu"
-})
+// array.map(item => {
+//     item.innerHTML += " C eh loco tiu"
+// })
+
+
+//getElementByTagName
+
+const el = [...document.getElementsByTagName("div")]
+document.body.style = "display: flex; gap: 3rem"
+
+console.log(el)
+console.log(array)
+
