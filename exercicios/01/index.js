19df00cb230b67a8790e593890457d4d4cad712a
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-18 22:12:12 -0300

    code refactor 2

--- exercicios/01/index.js
+++ exercicios/01/index.js
@@ -1,17 +1,18 @@
 const arr = [...document.querySelectorAll(".el")]
-const moveBtn = document.querySelector(".move")
-const box1 = document.querySelector("#c1")
-const box2 = document.querySelector("#c2")
-let b2Item
 
 arr.map(item => item.addEventListener("click", () => {
     item.classList.toggle("strong")
 }))
 
+const moveBtn = document.querySelector(".move")
+
+const box1 = document.querySelector("#c1")
+const box2 = document.querySelector("#c2")
+
 moveBtn.addEventListener("click", () => {
 
     const strongs = [...document.querySelectorAll(".strong")]
-    
+
     strongs.map(item => { 
         item.parentElement.id == "c1" 
         ? box2.appendChild(item) 
