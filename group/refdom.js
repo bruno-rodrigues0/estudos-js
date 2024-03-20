87db815023bc82f81d6823ed19cf6c43a6cfe9d2
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-20 14:18:54 -0300

    exercicio add item to list

--- group/refdom.js
+++ group/refdom.js
@@ -1,3 +1,58 @@
 const caixa1 = document.querySelector("#caixa1")
-const btn_c1 = document.querySelector("#c1")
+const btn_c = [...document.querySelectorAll(".curso")]
+const c1_2 = document.querySelector("#c1_2")
+
+let elements = []
+
+const refresh = () => {
+    const input = document.querySelector("input")
+    if(elements.length < 7 && input.value.trim() != ''){
+
+    let newElement = document.createElement("div")
+    
+    newElement.setAttribute("class", "curso c1")
+    newElement.innerHTML = input.value
+
+    caixa1.appendChild(newElement)
+
+    elements = [...document.querySelectorAll(".curso")]
+    input.value = ''
+
+   }else{
+    return
+   }
+}
+
+const add = document.querySelector("button")
+
+add.addEventListener("click", () => {
+    refresh()
+})
+
+
+
+// console.log(btn_c1.parentElement)
+// console.log(btn_c1.nextSibling)
+// console.log(btn_c1.lastElementChild)
+// console.log(btn_c1.children)
+// console.log(btn_c1.lastChild)
+
+
+// console.log(caixa1.hasChildNodes())
+// console.log(btn_c[0].hasChildNodes())
+// console.log(btn_c[0].childNodes)  //returns NodeList ""
+// console.log(btn_c[0].children)
+
+// if(caixa1.children.length > 0){
+//     console.log("possui filhos")
+// }else{
+//     console.log("nao possui filhos")
+// }
+
+// console.log(btn_c[0].children.length 
+// ? "possui filhos" 
+// : "nao possui filhos")
+
+// caixa1.firstElementChild.innerHTML = "ARROI'S DOCE"
+
 
