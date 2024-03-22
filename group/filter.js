1de0e45eab3210a9ebad69bf178419b54879e3a5
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-22 08:07:03 -0300

    aa

---
+++ group/filter.js
@@ -0,0 +1,8 @@
+const idades = [15, 21, 30, 17, 18, 44, 12, 50]
+
+const maior = idades.filter(item => item >= 18)
+const menor = idades.filter(item => item <= 18)
+
+console.log(idades)
+console.log(maior)
+console.log(menor)
