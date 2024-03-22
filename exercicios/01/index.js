7d2442f84a76822f628fccbf0f0d815e2d28aa5d
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-22 16:01:26 -0300

    aa

--- exercicios/01/index.js
+++ exercicios/01/index.js
@@ -1,25 +1,27 @@
 const arr = [...document.querySelectorAll(".el")]
-
-arr.map(item => item.addEventListener("click", () => {
-    item.classList.toggle("strong")
-}))
-
 const moveBtn = document.querySelector(".move")
-
 const box1 = document.querySelector("#c1")
 const box2 = document.querySelector("#c2")
 
-moveBtn.addEventListener("click", () => {
+const toggleStrong = (item) => {
+    item.classList.toggle("strong")
+}
 
-    const strongs = [...document.querySelectorAll(".strong")]
-
+const moveItem = (strongs) => {
     strongs.map(item => { 
         item.parentElement.id == "c1" 
         ? box2.appendChild(item) 
         : box1.appendChild(item)
     })
-    
-    strongs.map(item => {
-        item.classList.remove("strong")
-    })
+    strongs.map(item => item.classList.remove("strong"))
+}
+
+arr.map(item => item.addEventListener("click", function () {
+    toggleStrong(this)
+}))
+
+moveBtn.addEventListener("click", () => {
+    const strongs = [...document.querySelectorAll(".strong")]
+    moveItem(strongs)
 })
+
