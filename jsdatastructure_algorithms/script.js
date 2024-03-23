22630afd22014e6da17535bc3d02e71584e5af10
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-23 15:17:12 -0300

    aa

--- jsdatastructure_algorithms/script.js
+++ jsdatastructure_algorithms/script.js
@@ -1,28 +1,53 @@
-class Book {
-    constructor(title, pages, isbn) {
-        this.title = title;
-        this.pages = pages;
-        this.isbn = isbn;
-        Book.prototype.printIsbn = function () {
-            console.log(this.isbn)
-        }
-    }
-}
-
-let book2 = {
-    name: "string"
-}
-
-// class Books{
-//     title = string;
-//     pages = number;
-//     isbn = number;
+// class Book {
+//     constructor(title, pages, isbn) {
+//         this.title = title;
+//         this.pages = pages;
+//         this.isbn = isbn;
+//         Book.prototype.printIsbn = function () {
+//             console.log(this.isbn)
+//         }
+//     }
 // }
 
-let book = new Book("jsdatastructure", "500", "18267")
-let otherbook = new Book("Se eu ficar", "354", "12387")
+// let book2 = {
+//     name: "string"
+// }
 
-console.log(book, typeof(book))
-console.log(otherbook, typeof(otherbook))
+// // class Books{
+// //     title = string;
+// //     pages = number;
+// //     isbn = number;
+// // }
 
-otherbook.printIsbn()
+// let book = new Book("jsdatastructure", "500", "18267")
+// let otherbook = new Book("Se eu ficar", "354", "12387")
+
+// console.log(book, typeof(book))
+// console.log(otherbook, typeof(otherbook))
+
+// otherbook.printIsbn()
+
+// document.write("Olá!")
+
+// const book = {
+//     name: "Learning JavaScript Data Structure and Algorithms"
+// };
+
+// console.log("You are reading " + book.name + ".,\nand this is a new line\nand so is this. ");
+
+// console.log(
+// `Your are reading ${book.name},
+// and this is a new line,
+// and so is this.`
+// )
+
+
+// function sum (){
+//     let array = []
+    
+//     for(item of arguments){
+//         array.push(item)
+//     }
+
+//     console.log(array)
+// }
