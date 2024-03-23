488a43d1a7941f53739754bea098cdf6305449f9
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-23 13:45:28 -0300

    learn to debug code on browser and vscode

--- debugger/src/main.js
+++ debugger/src/main.js
@@ -8,11 +8,8 @@ function write(phrase, type){
         case 1:
             console.log(phrase);
             break;
-        case 2:
-            alert(phrase);
-            break;
         default:
-            document.write(phrase);
+            alert(phrase);
             break;
     }
 }
