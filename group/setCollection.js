75830decebc21061fbc7f2cddc4fb011feed6365
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-27 13:48:53 -0300

    Set collection

---
+++ group/setCollection.js
@@ -0,0 +1,9 @@
+let set = new Set([
+    "O limbo do meninos sem olhos", 
+    "Cachecol",
+    "Antes e depois da tempestade"
+])
+
+set.add("Rainha dos nihos bichos da floresta")
+
+console.log(set.keys())
