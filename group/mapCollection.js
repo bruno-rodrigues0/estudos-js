c8f628c6a80460b67a19f2a9a726991f35275380
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-27 13:40:11 -0300

    Map collection

---
+++ group/mapCollection.js
@@ -0,0 +1,23 @@
+let map = new Map()
+
+map.set("cu", "cuzhino")
+map.set("cu2", "cuzhino2")
+map.set("cu3", "cuzhino3")
+
+console.log(map)
+
+// console.log(map.get("anal"))
+// console.log(map.values())
+// console.log(map.keys())
+// console.log(map.size)
+
+map.delete("cu2")
+
+console.log(map)
+
+map.clear()
+
+console.log(map)
+
+console.log(typeof(map))
+
