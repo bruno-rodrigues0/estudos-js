e1f31640fb4556188c5b33921ad480a65f5b34b1
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-29 15:06:56 -0300

    constructor function

---
+++ group/constructorFunction.js
@@ -0,0 +1,11 @@
+function Pessoa(name, age){
+    this.nome = nome,
+    this.age = age,
+
+    this.getName = () => {
+        return this.name
+    },
+    this.getAge = () => {
+        return this.age
+    }
+}
