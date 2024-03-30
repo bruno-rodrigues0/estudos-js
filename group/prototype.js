7323b2b4fcf15eaa03b1ffb2f679916cf7acd7fa
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-30 09:56:28 -0300

    prototype

---
+++ group/prototype.js
@@ -0,0 +1,21 @@
+class Nave {
+    constructor(energia) {
+        this.energia = energia;
+        this.disparos = 100;
+    }
+}
+
+const n1 = new Nave(100)
+
+Nave.prototype.disparar = function(){
+    this.disparos--
+}
+
+
+console.log(Nave.prototype)
+
+for(let i = 0; i < 43; i++){
+    n1.disparar()
+}
+
+console.log(n1.disparos)
