cf5f45d86c234abd0c807cba6aec8220866d607f
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-30 09:16:03 -0300

    sla

---
+++ group/convertingJSON.js
@@ -0,0 +1,11 @@
+const pessoa = {
+    nome : "Bruno",
+    github: "bruno-rodrigues0",
+    curso: "tecnico em informatica",
+    disciplinas: {
+        d1: "PSW",
+        d2: "ASW",
+        d3: "DataBase"
+    }
+}
+
