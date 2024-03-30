231ab18223a26cab62aaf470c30054a6a4188b6b
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-30 09:16:10 -0300

    pse

--- group/objects.js
+++ group/objects.js
@@ -1,27 +1,62 @@
 class Pessoa{
+    
+    #name;
+    #age;
+    #socialStatus
     constructor(name, age, socialStatus){
-        this.name = name ?? "Não informado"
-        this.age = age ?? "Não informado"
-        this.socialStatus = socialStatus ?? "Não informado"
+        this.#name = name; 
+        this.#age = age; 
+        this.#socialStatus = socialStatus;
     }
+
+    get getName(){
+        return this.#name;
+    }
+
+    /**
+     * @param {string} name 
+     */
+
+    set setName(name){
+        this.#name = name;
+    }
+    get getAge(){
+        return this.#age;
+    }
+
+    /**
+     * @param {number} age
+     */
+
+    set setAge(age){
+        this.#age = age;
+    }
+
+    
 }
 
 let p1 = new Pessoa("Gabriel", 17, "Solteiro")
 
+p1.setName = 1
 
-class Car{
-        
-    itemType = "car"
+console.log(p1.getName)
 
-    constructor(name, type){
-        this.name = name ?? "Não informado"
-        name == "Supra" ? this.brand = "Toyota" : this.brand = "Other"
-        type == 1 ? this.type = "Esportivo" : this.type = "Comum"
-    }
 
-    getCar() {
-        console.log(`O carro é um ${this.brand == "Toyota" ? "Toyota" : ""} ${this.name} do tipo ${this.type}`)
-    }
-}
 
-let c1 = new Car("Supra", 1)
+
+
+
+// class Car{
+
+//     constructor(name, type){
+//         this.name = name ?? "Não informado"
+//         name == "Supra" ? this.brand = "Toyota" : this.brand = "Other"
+//         type == 1 ? this.type = "Esportivo" : this.type = "Comum"
+//     }
+
+//     getCar() {
+//         console.log(`O carro é um ${this.brand == "Toyota" ? "Toyota" : ""} ${this.name} do tipo ${this.type}`)
+//     }
+// }
+
+// let c1 = new Car("Supra", 1)
