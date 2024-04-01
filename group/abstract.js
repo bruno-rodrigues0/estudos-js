4fd192d2354b34ff3349f806091e2118b43cddff
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-04-01 11:21:59 -0300

    abstract

--- group/abstract.js
+++ group/abstract.js
@@ -10,7 +10,7 @@ class Pessoa{
         if(this.constructor === Pessoa){
             throw new TypeError("Essa classe não pode ser instanciada");
         }
-        this.CFP = CPF;
+        this.CPF = CPF;
         this.nome = nome;
         this.idade = idade;
     }
@@ -21,27 +21,30 @@ class Funcionario extends Pessoa{
     cargo;
 
     constructor(CPF, nome, idade, cargo){
+        if(!CPF){
+            throw new TypeError("É obrigatorio informar o CPF.");
+        }
         super(CPF, nome, idade);
         this.cargo = cargo;
     }
 };
 
-let funcionario = new Funcionario(10079345654, 'Bruno', 17);
+let funcionario = new Funcionario(10079345654, 'Bruno', 17, 'Estagiorio');
 
 console.log(funcionario);
 
 // let pessoa = new Pessoa(10079345654, 'Bruno', 17);
 
 
-let lol = new Function('a', 'b', `
-    if(a > b){
-        console.log("A eh maior que B");
-    } else {
-        console.log("B eh maior doq A");
-    }       
+// let lol = new Function('a', 'b', `
+//     if(a > b){
+//         console.log("A eh maior que B");
+//     } else {
+//         console.log("B eh maior doq A");
+//     }       
 
-    return;
-`);
+//     return;
+// `);
 
 
-console.log(lol(1, 3));
+// console.log(lol(1, 3));
