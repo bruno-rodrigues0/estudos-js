2933204b0690e869ee5d2af7174c227b5f31a989
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-04-03 13:26:00 -0300

    date

---
+++ group/date.js
@@ -0,0 +1,13 @@
+// let data = Date.now(); || data = new Data(); data.getTime() TimeStamp
+
+let data = new Date();
+
+console.log(data.getMonth());
+console.log(data.getFullYear());
+console.log(data.getMilliseconds());
+console.log(data.getTimezoneOffset());
+console.log(data.getVarDate);
+console.log(data.getDate());
+console.log(data);
+
+
