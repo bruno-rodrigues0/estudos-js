69f5d1d2739d575179a081358592ce9503b7353b
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-04-03 12:49:16 -0300

    calling promises

--- group/promises.js
+++ group/promises.js
@@ -1,29 +1,41 @@
 const numero =document.querySelector("#numero");
+const btn_promise = document.querySelector("#promise");
 
-let promise = new Promise((resolve, rejected) => {
-    let resultado = true;
-    let tempo = 3000;
+btn_promise.addEventListener('click', () =>  {
+    numero.innerHTML = 'Processando...';
+    console.log(promises())
+ 
+    promises()
+        .then((retorno) => {   
+            numero.innerHTML = retorno;
+            numero.classList.remove("erro");
+            numero.classList.add("ok");
+        })
+         .catch((retorno) => {
+            numero.innerHTML = retorno;
+            numero.classList.remove("ok");
+            numero.classList.add("erro");
+        }) 
 
-    setTimeout(() => {
-        if(resultado){
-            resolve('Deu certo');
-        } else {
-            rejected('Deu errado');
-        }
-    }, tempo);
+})
 
-});
+const promises = () => {
+    let promise = new Promise((resolve, rejected) => {
 
-promise
-    .then((retorno) => {   
-         numero.innerHTML = retorno;
-        numero.classList.remove("erro");
-        numero.classList.add("ok");
-    })
-    .catch((retorno) => {
-        numero.innerHTML = retorno;
-        numero.classList.remove("ok");
-        numero.classList.add("erro");
-     })
+        let resultado = false;
+        let tempo = 3000;
 
-numero.innerHTML = 'Processando...';
+        setTimeout(() => {
+            if(resultado){
+                resolve('Deu certo');
+            } else {
+                rejected('Deu errado');
+            }
+        }, tempo);
+        
+    }); 
+
+return promise;
+}
+
+
