8e087c1d0fa1835b52576c6c2b892f4cb191a3ed
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-04-03 11:33:25 -0300

    promise

--- group/promises.js
+++ group/promises.js
@@ -1,20 +1,29 @@
 const numero =document.querySelector("#numero");
 
-let resultado = false;
-let tempo = 3000;
+let promise = new Promise((resolve, rejected) => {
+    let resultado = true;
+    let tempo = 3000;
 
-setTimeout(() => {
-    resultado = true;
-}, tempo);
+    setTimeout(() => {
+        if(resultado){
+            resolve('Deu certo');
+        } else {
+            rejected('Deu errado');
+        }
+    }, tempo);
 
-if(resultado){
-    numero.innerHTML = "Deu tudo certo";
-    numero.classList.remove("erro");
-    numero.classList.add("ok")
-} else {
-    numero.innerHTML = "Deu tudo errado";
-    numero.classList.remove("ok");
-    numero.classList.add("erro")
-}
+});
+
+promise
+    .then((retorno) => {   
+         numero.innerHTML = retorno;
+        numero.classList.remove("erro");
+        numero.classList.add("ok");
+    })
+    .catch((retorno) => {
+        numero.innerHTML = retorno;
+        numero.classList.remove("ok");
+        numero.classList.add("erro");
+     })
 
 numero.innerHTML = 'Processando...';
