73b87e4d62c71be3a0e063648dde3a065aba6a05
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-04-08 17:06:09 -0300

    sla

--- jsdatastructure_algorithms/chapter06/linkedList.js
+++ jsdatastructure_algorithms/chapter06/linkedList.js
@@ -1,3 +1,4 @@
+import { Link } from "lucide";
 import { Node } from "./linked-list-models";
 
 function defaultEquals(a, b) {
@@ -34,4 +35,15 @@ export default class LinkedList{
 
         this.#count++;
     }
+
 }
+
+let list  = new LinkedList();
+
+list.push(1);
+list.push(2);
+list.push(3);
+list.push(4);
+
+
+console.log(list);
